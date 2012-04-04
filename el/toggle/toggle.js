60e8aebe30680a663e3d1e4214ab055c26c62583
Lauri Rooden <lauri@rooden.ee>
2012-04-04 15:09:22 +0300

    toggle_ios5

--- el/toggle/toggle.js
+++ el/toggle/toggle.js
@@ -1,7 +1,7 @@
 
 El.cache(
 	"toggle",
-	El.haml("%button.toggle.reset\n .anim\n  %b.on ON\n  %b OFF\n  %b.knob"),
+	El.haml("%button.reset.toggle\n .anim\n  %b.on ON\n  %b OFF\n  %b.knob"),
 	function(el){
 		el.on("mousedown touchstart", function(e){
 			Event.stop(e);
