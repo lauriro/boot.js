713a3f6a0bc1de271bf6bf62b765c57970f41166
Lauri Rooden <lauri@rooden.ee>
2012-10-24 15:03:42 +0300

    Add count to BitArray

--- bitarray.js
+++ bitarray.js
@@ -10,6 +10,18 @@ BitArray.prototype = {
 	get: function(bit) {
 		return 1 & (this.arr[bit>>>5] >>> (bit&0xff))
 	},
+	count: function() {
+		var v
+		, sum = 0, arr = this.arr, len = arr.length
+
+		while (len--) {
+			v = arr[len]
+			v = v - ((v >> 1) & 0x55555555)
+			v = (v & 0x33333333) + ((v >> 2) & 0x33333333)
+			sum += ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24
+		}
+		return sum
+	},
 	toString: function() {
 		return this.arr.map(function(val){
 			return ((val>>>0).toString(2).split("").reverse().join("")+"0000000000000000000000000000000").slice(0, 32)
