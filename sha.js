1eb04292e6de21d8b2d8467998018dd0cc1118f1
Lauri Rooden <lauri@rooden.ee>
2012-11-08 07:53:17 +0200

    Add pbkdf2 and rewrite hmac for internal integers

--- sha.js
+++ sha.js
@@ -15,36 +15,34 @@
 	function l(x, n) { // rotate left
 		return (x<<n) | (x>>>(32-n))
 	}
-	function r(x, n) { // rotate right
-		return (x>>>n) | (x<<(32-n))
+	function i2s(a) { // integer array to hex string
+		var i = a.length
+		while (i--) a[i] = ("0000000"+(a[i]>>>0).toString(16)).slice(-8)
+		return a.join("")
 	}
-	function i2s(i) { // int2str
-		return ("0000000"+(i>>>0).toString(16)).slice(-8)
+	function s2B(s) { // string to byte array
+		var a = unescape(encodeURIComponent(s)).split(""), i = a.length
+		while (i--) a[i] = a[i].charCodeAt()
+		return a
 	}
-	function s2i(s) { // str2int array
-		return unescape(encodeURIComponent(s)).split("").map(function(i){return i.charCodeAt()})
-	}
-
-	function sha_init(data) {
-		if (typeof(data) == "string") data = s2i(data)
-
-		var bin = [], i = 0, len = data.length
-		while (i < len) bin[i >> 2] = data[i++]<<24|data[i++]<<16|data[i++]<<8|data[i++]
-
-		i = len << 3
-		bin[len >> 2] |= 0x80 << (24 - (i & 31))
-		return bin.concat(nuls.slice( bin.length & 15 ), [0, i])
-	}
-	function sha_format(asBytes, a) {
-		if (asBytes) {
-			// convert to byte array
-			for (var out = [], i=0, len=a.length;i<len;i++) out.push( (a[i]>>>24)&0xff, (a[i]>>>16)&0xff, (a[i]>>>8)&0xff, a[i]&0xff );
-			return out
-		}
-		return a.map(i2s).join("")
+	function B2w(a) { // byte array ta integer array
+		var i = 0, bin = [], len = a.length
+		while (i < len) bin[i>>2] = a[i++]<<24|a[i++]<<16|a[i++]<<8|a[i++]
+		return bin
 	}
 
-	function sha1(data, asBytes) {
+	function sha_init(bin, len) {
+		if (typeof bin == "string") {
+			bin = s2B(bin)
+			len = bin.length
+			bin = B2w(bin)
+		} else if (!len) len = bin.length * 4
+
+		bin[len>>2] |= 0x80 << (24 - (31 & len<<3))
+		return bin.concat(nuls.slice( bin.length & 15 ), [0, len<<3])
+	}
+
+	function sha1(data, raw, _len) {
 		var a, b, c, d, e, t, j
 		, i = 0
 		, w = []
@@ -53,12 +51,12 @@
 		, C = 0x98badcfe
 		, D = 0x10325476
 		, E = 0xc3d2e1f0
-		, bin = sha_init(data)
+		, bin = sha_init(data, _len)
 		, len = bin.length
 
 		while (i < len) {
 			w = bin.slice(i, i+=(j=16))
-			while (j < 80) w[j++] = l(w[j-4]^w[j-9]^w[j-15]^w[j-17],1)
+			while (j < 80) w[j++] = l(w[j-4]^w[j-9]^w[j-15]^w[j-17], 1)
 			a = A
 			b = B
 			c = C
@@ -79,15 +77,20 @@
 			D += d
 			E += e
 		}
-		return sha_format(asBytes, [A, B, C, D, E])
+		t = [A, B, C, D, E]
+		return raw ? t : i2s(t)
+	}
+
+	S.sha1 = function(raw) {
+		return sha1(""+this, raw)
 	}
 
 
-	S.sha1 = function(asBytes) {
-		return sha1(""+this, asBytes)
+	function r(x, n) { // rotate right
+		return (x>>>n) | (x<<(32-n))
 	}
 
-	function sha256(data, asBytes) {
+	function sha256(data, raw, _len) {
 		var a, b, c, d, e, f, g, h, t1, t2, j
 		, i = 0
 		, w = []
@@ -99,9 +102,9 @@
 		, F = 0x9b05688c
 		, G = 0x1f83d9ab
 		, H = 0x5be0cd19
-		, bin = sha_init(data)
+		, bin = sha_init(data, _len)
 		, len = bin.length
-		, map = [ 
+		, K = [ 
 			0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5
 			, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174
 			, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da
@@ -129,7 +132,7 @@
 			h = H
 			j = 0
 			while (j < 64) {
-				t1 = h + (r(e,6)^r(e,11)^r(e,25)) + ((e&f)^((~e)&g)) + map[j] + w[j++]
+				t1 = h + (r(e,6)^r(e,11)^r(e,25)) + ((e&f)^((~e)&g)) + K[j] + w[j++]
 				t2 = (r(a,2)^r(a,13)^r(a,22)) + ((a&b)^(a&c)^(b&c))
 				h = g
 				g = f
@@ -149,29 +152,73 @@
 			G += g
 			H += h
 		}
-		return sha_format(asBytes, [A, B, C, D, E, F, G, H])
+		t = [A, B, C, D, E, F, G, H]
+		return raw ? t : i2s(t)
 	}
 
-	S.sha256 = function(asBytes) {
-		return sha256(""+this, asBytes)
+	S.sha256 = function(raw) {
+		return sha256(""+this, raw)
 	}
 
 	//** HMAC 
-	function hmac(hasher, blocksize, key, txt) {
-		var i = 0, ipad = [], opad = []
-		key = (key.length > blocksize) ? hasher(key, true) : s2i(key)
-		for(; i<blocksize; ipad[i]=key[i]^0x36, opad[i]=key[i++]^0x5c);
-		return hasher(opad.concat(hasher(ipad.concat(s2i(txt)),true)))
+	function hmac(hasher, blocksize, key, txt, raw) {
+		var i = 0, ipad = [], opad = [], txt_len
+		key = (key.length > 4*blocksize) ? hasher(key, "raw") : B2w(s2B(key))
+	
+		while (i<blocksize) {
+			ipad[i]=key[i]^0x36363636
+			opad[i]=key[i++]^0x5c5c5c5c
+		}
+
+		if (typeof txt == "string") {
+			txt = s2B(txt)
+			txt_len = txt.length
+			txt = B2w(txt)
+		} else txt_len = txt.length * 4
+		i = hasher(opad.concat(hasher(ipad.concat(txt), 1, 64 + txt_len)), 1)
+		return raw ? i : i2s(i)
 	}
 
 	S.hmac_sha1 = function(key) {
-		return hmac(sha1, 64, key, this)
+		return hmac(sha1, 16, key, ""+this)
 	}
 
 	S.hmac_sha256 = function(key) {
-		return hmac(sha256, 64, key, this)
+		return hmac(sha256, 16, key, ""+this)
 	}
+  //*/
+	
+	//** PBKDF2
+	String.pbkdf2 = function (password, salt, count, length) {
+		var u, ui, i, j, k
+		, out = []
+		, wlen = length>>2 || 5
+
+		/**
+		 * A minimum iteration count of 1,000 is recommended. 
+		 * For especially critical keys, 
+		 * or for very powerful systems 
+		 * or systems where user-perceived performance is not critical, 
+		 * an iteration count of 10,000,000 may be appropriate.
+		 */
+
+		count = count || 1000
+
+		for (k = 1; out.length < wlen; k++) {
+			u = ui = hmac(sha1, 16, password, salt+String.fromCharCode.call(null, k >> 24 & 0xF, k >> 16 & 0xF, k >>  8 & 0xF, k  & 0xF), 1)
+
+			for (i=1; i<count; i++) {
+				ui =  hmac(sha1, 16, password, ui, 1)
+				for (j=ui.length; j--;) u[j] ^= ui[j]
+			}
+
+			out = out.concat(u)
+		}
+		return i2s(out).slice(0, length*2 || 40)
+	}
+
 	//*/
+	
 
 }(String.prototype, [0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
 
@@ -216,9 +263,20 @@ TestCase("SHA").compare(
 , "a".hmac_sha256("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8551")
 , "4d7a8cb0bb07af2b8ef6273aa50852033eea4236c97934b58727f7ad68180ad4"
 , "String.hmac_sha256()"
+).compare(
+	String.pbkdf2("password", "salt", 1)
+, "0c60c80f961f0e71f3a9b524af6012062fe037a6"
+, String.pbkdf2("password", "salt", 2)
+, "ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957"
+//, String.pbkdf2("password", "salt", 4096)
+//, "4b007901b765489abead49d926f721d065a429c1"
+//, String.pbkdf2("pass\0word", "sa\0lt", 4096, 16)
+//, "56fa6aa75548099dcc37d7f03425e0c3"
+//, String.pbkdf2("passwordPASSWORDpassword", "saltSALTsaltSALTsaltSALTsaltSALTsalt", 4096, 25)
+//, "3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038"
+, "String.pbkdf2"
 ).done()
 
 //*/
-
 
 
