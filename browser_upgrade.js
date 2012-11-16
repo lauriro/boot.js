349474992bc6ea7b1c6b9accd4f5e3a069cc9030
Lauri Rooden <lauri@rooden.ee>
2012-11-16 11:49:16 +0200

    Move base64 into browser_upgrade

--- browser_upgrade.js
+++ browser_upgrade.js
@@ -76,7 +76,35 @@
 
 	I(Date[P], "toISOString", "return this.format('isoUtcDateTime')")
 
+	//** base64
+	if (!("atob" in w)) {
+		var ba = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("")
+		, bm = {"=":0}
+		
+		for (i = 64; bm[ba[--i]]=i;);
 
+		// base64_encode
+		w.btoa = function(s) {
+			for (var out=[],b,i=0,len=s.length;i<len;) {
+				b = s.charCodeAt(i++)<<16 | s.charCodeAt(i++)<<8 | s.charCodeAt(i++)
+				out.push(ba[b>>18&0x3f], ba[b>>12&0x3f], ba[b>>6&0x3f], ba[b&0x3f])
+			}
+			if (len%=3) out.splice(len-3, 2, len==1?"==":"=")
+			return out.join("")
+		}
+
+		// base64_decode
+		w.atob = function(s) {
+			for (var out=[],b,i=0,len=s.length,s=s.split("");i<len;) {
+				b = bm[s[i++]]<<18 | bm[s[i++]]<<12 | bm[s[i++]]<<6 | bm[s[i++]]
+				out.push(b>>16 & 0xff, b>>8 & 0xff, b & 0xff)
+			}
+			if (s[len-1] == "=") out.length -= s[len-2] == "=" ? 2 : 1
+			return String.fromCharCode.apply(null, out)
+		}
+	}
+
+	//*/
 
 	// XMLHttpRequest was unsupported in IE 5.x-6.x
 	// MSXML version 3.0 was the last version of MSXML to support version-independent ProgIDs.
@@ -297,4 +325,25 @@ test.done();
 
 
 
+/** Tests for base64
+!function(){
+	var map = {
+		"b": "Yg==",
+		"ba": "YmE=",
+		"bas": "YmFz",
+		"base64 encode": "YmFzZTY0IGVuY29kZQ==",
+		"{@:#}": "e0A6I30=",
+		"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.": "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVlciBhZGlwaXNjaW5nIGVsaXQsIHNlZCBkaWFtIG5vbnVtbXkgbmliaCBldWlzbW9kIHRpbmNpZHVudCB1dCBsYW9yZWV0IGRvbG9yZSBtYWduYSBhbGlxdWFtIGVyYXQgdm9sdXRwYXQuIFV0IHdpc2kgZW5pbSBhZCBtaW5pbSB2ZW5pYW0sIHF1aXMgbm9zdHJ1ZCBleGVyY2kgdGF0aW9uIHVsbGFtY29ycGVyIHN1c2NpcGl0IGxvYm9ydGlzIG5pc2wgdXQgYWxpcXVpcCBleCBlYSBjb21tb2RvIGNvbnNlcXVhdC4gRHVpcyBhdXRlbSB2ZWwgZXVtIGlyaXVyZSBkb2xvciBpbiBoZW5kcmVyaXQgaW4gdnVscHV0YXRlIHZlbGl0IGVzc2UgbW9sZXN0aWUgY29uc2VxdWF0LCB2ZWwgaWxsdW0gZG9sb3JlIGV1IGZldWdpYXQgbnVsbGEgZmFjaWxpc2lzIGF0IHZlcm8gZXJvcyBldCBhY2N1bXNhbiBldCBpdXN0byBvZGlvIGRpZ25pc3NpbSBxdWkgYmxhbmRpdCBwcmFlc2VudCBsdXB0YXR1bSB6enJpbCBkZWxlbml0IGF1Z3VlIGR1aXMgZG9sb3JlIHRlIGZldWdhaXQgbnVsbGEgZmFjaWxpc2kuIE5hbSBsaWJlciB0ZW1wb3IgY3VtIHNvbHV0YSBub2JpcyBlbGVpZmVuZCBvcHRpb24gY29uZ3VlIG5paGlsIGltcGVyZGlldCBkb21pbmcgaWQgcXVvZCBtYXppbSBwbGFjZXJhdCBmYWNlciBwb3NzaW0gYXNzdW0uIFR5cGkgbm9uIGhhYmVudCBjbGFyaXRhdGVtIGluc2l0YW07IGVzdCB1c3VzIGxlZ2VudGlzIGluIGlpcyBxdWkgZmFjaXQgZW9ydW0gY2xhcml0YXRlbS4gSW52ZXN0aWdhdGlvbmVzIGRlbW9uc3RyYXZlcnVudCBsZWN0b3JlcyBsZWdlcmUgbWUgbGl1cyBxdW9kIGlpIGxlZ3VudCBzYWVwaXVzLiBDbGFyaXRhcyBlc3QgZXRpYW0gcHJvY2Vzc3VzIGR5bmFtaWN1cywgcXVpIHNlcXVpdHVyIG11dGF0aW9uZW0gY29uc3VldHVkaXVtIGxlY3RvcnVtLiBNaXJ1bSBlc3Qgbm90YXJlIHF1YW0gbGl0dGVyYSBnb3RoaWNhLCBxdWFtIG51bmMgcHV0YW11cyBwYXJ1bSBjbGFyYW0sIGFudGVwb3N1ZXJpdCBsaXR0ZXJhcnVtIGZvcm1hcyBodW1hbml0YXRpcyBwZXIgc2VhY3VsYSBxdWFydGEgZGVjaW1hIGV0IHF1aW50YSBkZWNpbWEuIEVvZGVtIG1vZG8gdHlwaSwgcXVpIG51bmMgbm9iaXMgdmlkZW50dXIgcGFydW0gY2xhcmksIGZpYW50IHNvbGxlbW5lcyBpbiBmdXR1cnVtLg=="
+	}
+
+	var test = TestCase("Base64");
+	
+	for (var key in map) if (map.hasOwnProperty(key)) test.compare(btoa(key), map[key], "base64_encode btoa");
+	for (var key in map) if (map.hasOwnProperty(key)) test.compare(key, atob(map[key]), "base64_decode atob");
+	
+	test.done();
+
+}();
+
+//*/
 
--- min.js
+++ min.js
@@ -22,6 +22,16 @@ b+="if(a.call(b,t[i],i,t))"
 I(A,"filter",b+"o.push(t[i])"+c)
 I(A,"some",b+"return!0;return!1")
 I(Date[P],"toISOString","return this.format('isoUtcDateTime')")
+if(!("atob"in w)){var ba="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split(""),bm={"=":0}
+for(i=64;bm[ba[--i]]=i;);
+w.btoa=function(s){for(var out=[],b,i=0,len=s.length;i<len;){b=s.charCodeAt(i++)<<16|s.charCodeAt(i++)<<8|s.charCodeAt(i++)
+out.push(ba[b>>18&0x3f],ba[b>>12&0x3f],ba[b>>6&0x3f],ba[b&0x3f])}
+if(len%=3)out.splice(len-3,2,len==1?"==":"=")
+return out.join("")}
+w.atob=function(s){for(var out=[],b,i=0,len=s.length,s=s.split("");i<len;){b=bm[s[i++]]<<18|bm[s[i++]]<<12|bm[s[i++]]<<6|bm[s[i++]]
+out.push(b>>16&0xff,b>>8&0xff,b&0xff)}
+if(s[len-1]=="=")out.length-=s[len-2]=="="?2:1
+return String.fromCharCode.apply(null,out)}}
 I(w,"XMLHttpRequest","return new ActiveXObject('MSXML2.XMLHTTP')")
 w.xhr=function(method,url,cb,sync){var r=xhrs.shift()||new XMLHttpRequest
 r.open(method,url,!sync)
