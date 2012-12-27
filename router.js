940f019b73bbb2b650528cf46dc30d2817fd07cb
Lauri Rooden <lauri@rooden.ee>
2012-12-27 15:47:15 +0200

    Router api update

--- router.js
+++ router.js
@@ -16,59 +16,61 @@
 			, i = 0
 			, junks = path.replace(/\/+$/, "").split("/")
 			, route = junks[0]
-			, last = junks.length - 1
+			, last = junks.length
 			, names = {}
 			, obj = { cb: fn, names: names }
 
 			while (true) {
-				if (i == last || junks[i+1].charAt(0) == "*") {
+				junk = junks[++i]
+
+				if (i == last || junk.charAt(junk.length - 1) == "?") {
 					t.map[route] = obj
 					t.stat[route] = 0
 				}
 
-				if (++i > last) break
+				if (i >= last) break
 				
-				junk = junks[i]
-				if (junk.charAt(0) == ":" || junk.charAt(0) == "*") {
-					names[i] = junk.slice(1)
+				if (junk.charAt(0) == ":" || junk.charAt(junk.length - 1) == "?") {
+					names[i] = junk.replace(/^:+|\?+$/g, "")
 					junk = ":?"
 				}
 				route += "/" + junk
 			}
 		},
 
-		route: function ex(route, cb, scope) {
-			var key = ""
+		route: function ex(path, cb, scope) {
+			var junks, route
+			, i = 0
+			, key = ""
 			, map = this.map
 
 			// Fast case
-			if (map[route]) {
-				key = route
-				route = ""
+			if (map[path]) {
+				key = path
+				path = ""
 			} else {
-				var junks = route.split("/")
-				, i = 0
-				, next = junks[0]
-
+				junks = path.split("/")
+				route = junks[0]
 
 				while (true) {
-					key = next
-					next += "/" + junks[++i]
+					key = route
+					route += "/" + junks[++i]
 
-					if (!map.hasOwnProperty(next)) {
-						next = key + "/:?"
-						if (map.hasOwnProperty(next)) {
-							(scope.args || (scope.args = {}))[ map[next].names[i] ] = junks[i]
+					if (!map.hasOwnProperty(route)) {
+						route = key + "/:?"
+						if (map.hasOwnProperty(route)) {
+							(scope.args || (scope.args = {}))[ map[route].names[i] ] = junks[i]
 						} else {
+							// TODO:2012-12-28: Should we add route to map for cache?
 							break
 						}
 					}
 				}
-				route = junks.slice(i).join("/")
+				path = junks.slice(i).join("/")
 			}
 
 			this.stat[key]++
-			map[key].cb(route, cb, scope )
+			map[key].cb.call(scope, path, cb)
 		}
 	}
 
@@ -79,15 +81,16 @@
 
 /** Tests
 !function(){
-	var x
+	var x, undef
 	, scope = {}
 	, test = new TestCase("Router")
 	, r = new Router()
 
 
+	r.add("", function(){x = 0})
 	r.add("/api/", function(){x = 1})
 	r.add("/api/v1", function(){x = 2})
-	r.add("/api/v2/*method", function(){x = 3})
+	r.add("/api/v2/:method?", function(){x = 3})
 
 	r.route("/api/")
 	test.compare( x , 1 , "Router.route")
@@ -96,8 +99,13 @@
 	r.route("/api/v2")
 	test.compare( x , 3)
 	r.route("/api/v2/hello", null, scope)
-	test.compare( x , 3)
+	test.compare( x , 3, scope.args.method, "hello")
+	r.route("/apv2")
+	test.compare( x , 0)
 
 	test.done()
 }()
 //*/
+
+
+
