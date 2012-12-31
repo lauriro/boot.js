56fc7d21c8af39fa137c62983cf88c2dfade932e
Lauri Rooden <lauri@rooden.ee>
2012-12-31 15:50:43 +0200

    Router cleanup

--- router.js
+++ router.js
@@ -4,75 +4,83 @@
 !function(exports) {
 
 	function Router() {
-		this.map = { "": { cb: function(){} } }
+		this.map = { "": function(){} }
 		this.stat = { "": 0 }
 	}
 
-	Router.prototype = {
-		add: function(path, fn) {
+	function RouterAdd(path, fn, opts) {
 
-			var junk
-			, t = this
-			, i = 0
-			, junks = path.replace(/\/+$/, "").split("/")
-			, route = junks[0]
-			, last = junks.length
-			, names = {}
-			, obj = { cb: fn, names: names }
+		var junk
+		, t = this
+		, i = 0
+		, junks = path.replace(/\/+$/, "").split("/")
+		, route = junks[0]
+		, last = junks.length
+		, names = {}
+		, obj = fn
 
-			while (true) {
-				junk = junks[++i]
+		while (true) {
+			junk = junks[++i]
 
-				if (i == last || junk.charAt(junk.length - 1) == "?") {
-					t.map[route] = obj
-					t.stat[route] = 0
-				}
-
-				if (i >= last) break
-				
-				if (junk.charAt(0) == ":" || junk.charAt(junk.length - 1) == "?") {
-					names[i] = junk.replace(/^:+|\?+$/g, "")
-					junk = ":?"
-				}
-				route += "/" + junk
-			}
-		},
-
-		route: function ex(path, cb, scope) {
-			var junks, route
-			, i = 0
-			, key = ""
-			, map = this.map
-
-			// Fast case
-			if (map[path]) {
-				key = path
-				path = ""
-			} else {
-				junks = path.split("/")
-				route = junks[0]
-
-				while (true) {
-					key = route
-					route += "/" + junks[++i]
-
-					if (!map.hasOwnProperty(route)) {
-						route = key + "/:?"
-						if (map.hasOwnProperty(route)) {
-							(scope.args || (scope.args = {}))[ map[route].names[i] ] = junks[i]
-						} else {
-							// TODO:2012-12-28: Should we add route to map for cache?
-							break
-						}
-					}
-				}
-				path = junks.slice(i).join("/")
+			if (i == last || junk.charAt(junk.length - 1) == "?") {
+				t.map[route] = obj
+				t.stat[route] = 0
 			}
 
-			this.stat[key]++
-			map[key].cb.call(scope, path, cb)
+			if (i >= last) break
+
+			if (junk.charAt(0) == ":" || junk.charAt(junk.length - 1) == "?") {
+				names[i] = junk.replace(/^:+|\?+$/g, "")
+				junk = ":?"
+				if (obj === fn) {
+					obj = { cb: fn, names: names, opts: opts || {} }
+				}
+			}
+			route += "/" + junk
 		}
 	}
+
+	function RouterRoute(path, cb, scope) {
+		var junks
+		, i = 0
+		, route = ""
+		, map = this.map
+
+		// Fast case
+		if (map[path]) {
+			route = path
+			path = ""
+		} else {
+			junks = path.split("/")
+			path = junks[0]
+
+			while (true) {
+				route = path
+				path += "/" + junks[++i]
+
+				if (!map.hasOwnProperty(path)) {
+					path = route + "/:?"
+					if (map.hasOwnProperty(path)) {
+						(scope.args || (scope.args = {}))[ map[path].names[i] ] = junks[i]
+					} else {
+
+						// TODO:2012-12-28: Should we add route to map for cache?
+						break
+					}
+				}
+			}
+			path = junks.slice(i).join("/")
+		}
+
+		this.stat[route]++
+		;(map[route].cb || map[route]).call(scope, path, cb)
+	}
+
+
+	Router.prototype = 
+		{ add: RouterAdd
+		, route: RouterRoute
+		}
 
 	exports.Router = Router
 }(this)
@@ -98,8 +106,13 @@
 	test.compare( x , 2)
 	r.route("/api/v2")
 	test.compare( x , 3)
+
 	r.route("/api/v2/hello", null, scope)
 	test.compare( x , 3, scope.args.method, "hello")
+	
+	r.route("/api/v2/hello", null, scope)
+	test.compare( x , 3, scope.args.method, "hello")
+	
 	r.route("/apv2")
 	test.compare( x , 0)
 
